33fd3599701d60848f52d901d10b3d9afb4d50c1
ewan-m <ewanmorrrison@gmail.com>
2019-11-23 16:03:11 +0000

    IE11 sucks and I'm not gonna change

--- routing/router.js
+++ routing/router.js
@@ -1,23 +1,22 @@
-function replaceInnerHtmlWithNewPage(response, element) {
+const replaceInnerHtmlWithNewPage = (response, element) => {
     if (response.readyState === 4 && response.status === 200) {
         element.innerHTML = response.responseText;
     }
 };
 
-function loadPage(pageName, routerOutletId) {
+const loadPage = (pageName, routerOutletId) => {
     const element = document.getElementById(routerOutletId);
     const request = new XMLHttpRequest();
-    request.onload = function() { replaceInnerHtmlWithNewPage(request, element); }
+    request.onload = () => replaceInnerHtmlWithNewPage(request, element);
     request.open("GET", `pages/${pageName}.html`, true);
     request.send();
 };
 
-function getNewRoute(routes, defaultRoute) {
-    return routes.find(route => window.location.hash.replace("#", "") === route) ||
+const getNewRoute = (routes, defaultRoute) =>
+    routes.find(route => window.location.hash.replace("#", "") === route) ||
     defaultRoute;
-}
 
-export function enableRouting(routes, defaultRoute, routerOutletId) {
+export const enableRouting = (routes, defaultRoute, routerOutletId) => {
     loadPage(getNewRoute(routes, defaultRoute), routerOutletId);
     window.addEventListener("hashchange", () =>
         loadPage(getNewRoute(routes, defaultRoute), routerOutletId)
